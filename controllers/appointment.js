f2744178e249cddab81cda8a49ef71c53987a402
Dipesh <dipeshshtha4@gmail.com>
2023-04-04 15:03:08 +0545

    Maze Commit

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -19,7 +19,7 @@ export const createAppointment = async (req, res) => {
 export const getAppointment = async (req, res) => {
   try {
     const appointment = await appointmentModel.find({ userId: req.userId });
-    res.status(201).json(appointment);
+    res.status(201).json({ appointment });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
