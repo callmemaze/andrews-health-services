3cdb2140477bafe6eb5b63ff1e060e48450940c8
Dipesh <dipeshshtha4@gmail.com>
2023-05-01 12:53:49 +0545

    Maze Commit

--- controllers/report.js
+++ controllers/report.js
@@ -3,22 +3,10 @@ import reportModel from "../models/report.js";
 
 export const generateReport = async (req, res) => {
   try {
-    const report = await reportModel.find({ userId: req.userId });
+    const report = await reportModel
+      .find({ userId: req.userId })
+      .sort({ _id: -1 });
     res.status(200).json({ report });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
-
-export const createReport = async (req, res) => {
-  try {
-    const report = req.body;
-    const newReport = new reportModel({
-      ...report,
-      userId: req.userId,
-    });
-    await newbooking.save();
-    res.status(201).json(newReport);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
